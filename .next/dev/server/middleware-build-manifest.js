72f3a50c32e1ffbc69d626b30a1289625446fbb1
Anirudh S <156110036+ANAGHAKTP@users.noreply.github.com>
2026-02-01 22:56:34 +0530

    feat: Add .gitignore and define root CSS variables in globals.css, updating Next.js development build artifacts.

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -3,22 +3,9 @@ globalThis.__BUILD_MANIFEST = {
     "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [
-    "static/chunks/f614f_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_a769d04c._.js",
-    "static/chunks/f614f_next_dist_compiled_react-dom_fdc15ed9._.js",
-    "static/chunks/f614f_next_dist_compiled_react-server-dom-turbopack_66dc959a._.js",
-    "static/chunks/f614f_next_dist_compiled_next-devtools_index_ccaaa50f.js",
-    "static/chunks/f614f_next_dist_compiled_1c8ed1b2._.js",
-    "static/chunks/f614f_next_dist_client_9643b1fb._.js",
-    "static/chunks/f614f_next_dist_e9c21b2b._.js",
-    "static/chunks/f614f_@swc_helpers_cjs_5bcdaaca._.js",
-    "static/chunks/Anagha_Portfolio_a0ff3932._.js",
-    "static/chunks/turbopack-Anagha_Portfolio_032b15fb._.js"
-  ]
+  "rootMainFiles": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
